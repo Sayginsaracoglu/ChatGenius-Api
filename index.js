0d1c1a5b15f8c8a04385ab338cb6a301e4c1d4d2
Sayginsaracoglu <93162838+Sayginsaracoglu@users.noreply.github.com>
2023-03-24 04:38:58 -0400

    20

--- index.js
+++ index.js
@@ -24,17 +24,13 @@ app.listen(PORT,()=>{
 
 async function createConversation(messageHistory){
    
-    console.log(typeof messageHistory);
-    console.log(messageHistory)
-    console.log('last' + messageHistory[messageHistory.length() - 1 ].content)
-    //let lastUserInput = messageHistory[messageHistory.length - 1].content;
-    //console.log(`last user input ${lastUserInput}`)
-    // let response = await openai.createModeration({
-    //     input : lastUserInput
-    // })
-    // const flagged = response.results[0].flagged;
-
-   // console.log("flagged " + flagged)
+    let lastUserInput = messageHistory[messageHistory.length() - 1].content;
+    
+     let response = await openai.createModeration({
+         input : lastUserInput
+     })
+     const flagged = response.results[0].flagged;
+     console.log("flagged " + flagged)
     let completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: messageHistory,
