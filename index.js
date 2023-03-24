c94d97d03ebef0ad9281096b469a5274d790b314
Sayginsaracoglu <93162838+Sayginsaracoglu@users.noreply.github.com>
2023-03-24 04:14:13 -0400

    11

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ app.listen(PORT,()=>{
 
 async function createConversation(messageHistory){
    
-    let lastUserInput = messageHistory[messageHistory.length - 1].content;
 
-    let response =await openai.createModeration({
+    let lastUserInput = messageHistory[messageHistory.length - 1].content;
+    console.log(`last user input ${lastUserInput}`)
+    let response = await openai.createModeration({
         input : lastUserInput
     })
     const flagged = response.results[0].flagged;
